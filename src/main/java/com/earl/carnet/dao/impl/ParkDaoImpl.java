eecb2d70bf1e9798c7f4ecb6b870d30482ce7176
superGG <459714770@qq.com>
2016-06-12 10:24:22 +0800

    添加停车场功能

---
+++ src/main/java/com/earl/carnet/dao/impl/ParkDaoImpl.java
@@ -0,0 +1,25 @@
+package com.earl.carnet.dao.impl;
+
+import com.earl.carnet.commons.dao.impl.BaseDaoImpl;
+import com.earl.carnet.dao.ParkDao;
+import com.earl.carnet.domain.carnet.Park.Park;
+import com.earl.carnet.domain.carnet.RepairShop.RepairShop;
+import org.beetl.sql.core.SQLReady;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("ParkDao")
+public class ParkDaoImpl extends BaseDaoImpl<Park> implements ParkDao {
+
+    @Override
+    public List<Park> getAroundPark(Double lat, Double lon) {
+        //经纬度 1度 = 111千米. 0.027 = 3 千米
+        //获取做坐标点周围3千米范围内的停车场
+        String range = "0.027";
+        String sql = "select * from park r where r.lat<?+" + range + " and r.lat>?-" + range + " and r.lon <?+" + range + " and r.lon>?-" + range;
+        SQLReady sqlReady = new SQLReady(sql, lat, lat, lon, lon);
+        List<Park> list = sqlManager.execute(sqlReady, Park.class);
+        return list;
+    }
+}
---
+++ src/main/java/com/earl/carnet/web/ParkController.java
@@ -0,0 +1,131 @@
+package com.earl.carnet.web;
+
+import com.earl.carnet.commons.vo.ResultMessage;
+import com.earl.carnet.domain.carnet.Park.Park;
+import com.earl.carnet.domain.carnet.park.park;
+import com.earl.carnet.domain.carnet.brand.Brand;
+import com.earl.carnet.exception.DomainSecurityException;
+import com.earl.carnet.service.ParkService;
+import com.earl.carnet.service.parkService;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/park")
+// @Api(value="Person Rest Service") //修改生成的son路径
+public class ParkController extends BaseController {
+
+    private final Logger log = LoggerFactory
+            .getLogger(ParkController.class);
+
+    @Autowired
+    private ParkService parkService;
+
+    private ResultMessage result = null;
+
+    /**
+     * GET /park -> get all the park
+     */
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "得到所有停车场信息", notes = "find All park", httpMethod = "GET", response = park.class, responseContainer = "List")
+    public ResultMessage getAll() {
+        log.debug("REST request to get all park");
+        result = new ResultMessage();
+        result.getResultParm().put("park", parkService.findAll());
+        return result;
+    }
+
+    @Valid
+    @RequestMapping(value = "/getAroundPark", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "获取周围停车场信息", notes = "get user's around park", httpMethod = "GET", response = park.class, responseContainer = "List")
+    public ResultMessage getAroundShop(
+            @NotNull(message = "lon不能为空")
+            @ApiParam(required = true, name = "lon", value = "纬度坐标")
+            Double lon,
+            @NotNull(message = "lat不能为空")
+            @ApiParam(required = true, name = "lat", value = "经度坐标")
+            Double lat) {
+        result = new ResultMessage();
+        List<Park> shopList = parkService.getAroundPark(lat, lon);
+        result.getResultParm().put("park", shopList);
+        if (shopList.size() == 0) throw new DomainSecurityException("周围3公里都没有加盟停车场");
+        return result;
+    }
+
+    /**
+     * GET /park -> get park by id
+     */
+    @Valid
+    @RequestMapping(value = "/getParkById={id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "得到停车场详情", notes = "get park by id", httpMethod = "GET", response = park.class, responseContainer = "List")
+    public ResultMessage getParkById(
+            @PathVariable
+            @ApiParam(required = true, name = "id", value = "停车场id")
+            @NotNull(message = "id不能为空")
+            Long id) {
+        log.debug("REST request to get all Shop");
+        Park shop = parkService.findOne(id);
+        result = new ResultMessage();
+        if (shop != null) {
+            result.getResultParm().put("shop", shop);
+        } else {
+            result.setServiceResult(false);
+        }
+        return result;
+    }
+
+    /**
+     * POST /shop -> save shop
+     */
+    @RequestMapping(value = "/save", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "添加停车场信息", notes = "save shop", httpMethod = "POST", response = Brand.class, responseContainer = "List")
+    public ResponseEntity<ResultMessage> save(Park park) {
+        log.debug("save shop");
+        int id = parkService.insertBackId(park);
+        result = new ResultMessage();
+        result.getResultParm().put("park", parkService.findOne(id));
+        return new ResponseEntity<ResultMessage>(result, HttpStatus.OK);
+    }
+
+
+    /**
+     * 删除停车场.
+     *
+     * @return
+     */
+    @Valid
+    @RequestMapping(value = "/delete", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "根据id停车场信息", notes = "delete shop by id", httpMethod = "POST", response = String.class)
+    public ResponseEntity<ResultMessage> delete(
+            @ApiParam(required = true, name = "id", value = "停车场id")
+            @NotNull(message = "id不能为空")
+            Long id) {
+        result = new ResultMessage();
+        if (id == null) {
+            result.setResultInfo("id为空");
+            result.setServiceResult(false);
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        }
+        log.info("进入controller层删除停车场delete方法");
+        parkService.delete(id);
+        result.setResultInfo("删除停车场成功");
+        result.setServiceResult(true);
+        return new ResponseEntity<ResultMessage>(result, HttpStatus.OK);
+    }
+
+
+}
